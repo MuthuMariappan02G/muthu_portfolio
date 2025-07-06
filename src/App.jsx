0873787c442ac74268494cf45658942c330b0264
Muthumariappan <muthumariappan.ganapathi.gove.co>
2025-07-06 18:56:46 +0530

    Portfolio code moved

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Contact from "./component/Contact";
 import Home from "./component/Home";
 import Sidenav from "./component/SideBar";
 import Work from "./component/Work";
-import Projects from "./component/Projects";
+// import Projects from "./component/Projects";
 
 function App() {
   return (
